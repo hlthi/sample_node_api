f91a76efe51ff1473bedadfc80745489c85e610f
Muhammet Ozturk <mozturk@eriktronik.com>
2019-09-21 23:50:52 +0300

    split app file to app-index file

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mainMw from './main-mw.js'
+import errorMw from './error-mw.js'
+import db from './db.js'
+
+async function app () {
+  // connect mongo
+  await db()
+
+  // create express
+  const app = express()
+
+  // For each request, parse request body into a JavaScript object where header Content-Type is application/json
+  app.use(bodyParser.json())
+
+  // news router (secured)
+  app.post('/', mainMw)
+
+  // express error handler
+  app.use(errorMw)
+
+  return app.listen(3061)
+}
+
+export default app
--- src/index.js
+++ src/index.js
@@ -1,28 +1,4 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import mainMw from './main-mw.js'
-import errorMw from './error-mw.js'
-import db from './db.js'
-
-async function start () {
-  // connect mongo
-  await db()
-
-  // create express
-  const app = express()
-
-  // For each request, parse request body into a JavaScript object where header Content-Type is application/json
-  app.use(bodyParser.json())
-
-  // news router (secured)
-  app.post('/', mainMw)
-
-  // express error handler
-  app.use(errorMw)
-
-  app.listen(3061, () => console.log('API listening on port 3061!'))
-}
-
+import start from './app.js'
 /**
  * Start and handle error
  */
