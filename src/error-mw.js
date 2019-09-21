f1d8eccaa612e810859d8b7034fac29ccfbdd70e
Muhammet Ozturk <mozturk@eriktronik.com>
2019-09-21 18:30:14 +0300

    error middle ware

---
+++ src/error-mw.js
@@ -0,0 +1,14 @@
+import ResultModel from './ResultModel.js'
+
+function errorMw (err, req, res, next) {
+  const result = new ResultModel()
+  result.fail(err.output.statusCode, err.output.payload.message)
+
+  /**
+   * I dont like this. But document says that...
+   * HTTP have status code, why we break ?
+   */
+  return res.status(200).json(result)
+}
+
+export default errorMw
